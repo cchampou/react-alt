6139bc043f6b01abae6fdba15853c86da7bbddeb
cchampou <clement@champouillon.com>
2022-02-16 23:31:38 +0100

    chore: add documentation tool

--- src/hooks/useKeyPress.ts
+++ src/hooks/useKeyPress.ts
@@ -1,5 +1,11 @@
 import { useEffect } from 'react';
 
+/**
+ * Choose the key you want to respond and pass it an action, a function of any
+ * kind.
+ * @param {function} action
+ * @param {string} keyCode
+ */
 const useKeyPress = (action: () => unknown, keyCode: string) => {
   const keyHandler = (e: KeyboardEvent) => {
     const { key } = e;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 import useKeyPress from './hooks/useKeyPress';
 
-export const add = (a: number, b: number): number => a + b;
-
 export { useKeyPress };
